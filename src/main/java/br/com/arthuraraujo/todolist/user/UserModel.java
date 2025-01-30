bd40bdf88a8c2216fc0e8f9977299b98563d62df
arthur94a <arthuraraujo94@hotmail.com>
2025-01-30 15:58:56 -0300

    Validando username

--- src/main/java/br/com/arthuraraujo/todolist/user/UserModel.java
+++ src/main/java/br/com/arthuraraujo/todolist/user/UserModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 // import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -19,11 +20,9 @@ public class UserModel {
     @GeneratedValue(generator = "UUID")
     private UUID id;
     
-    // @Column(name = "username")
+    @Column(unique = true)
     private String username;
-    // @Column(name = "name")
     private String name;
-    // @Column(name = "password")
     private String password;
 
     @CreationTimestamp
