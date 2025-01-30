f1ffdcb90c0621de4b555736cfc3f9fa8e1cba55
arthur94a <arthuraraujo94@hotmail.com>
2025-01-29 22:25:06 -0300

    Criando a tabela de usuários

--- src/main/java/br/com/arthuraraujo/todolist/user/UserModel.java
+++ src/main/java/br/com/arthuraraujo/todolist/user/UserModel.java
@@ -1,11 +1,31 @@
 package br.com.arthuraraujo.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+// import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
     
+    // @Column(name = "username")
     private String username;
+    // @Column(name = "name")
     private String name;
+    // @Column(name = "password")
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
