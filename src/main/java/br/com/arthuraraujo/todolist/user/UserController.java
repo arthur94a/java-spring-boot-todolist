15e18f80e75dc811586d32b59f48b58d08b4d78b
arthur94a <arthuraraujo94@hotmail.com>
2025-01-30 10:14:19 -0300

    repository

--- src/main/java/br/com/arthuraraujo/todolist/user/UserController.java
+++ src/main/java/br/com/arthuraraujo/todolist/user/UserController.java
@@ -3,15 +3,20 @@ package br.com.arthuraraujo.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
     
     @PostMapping("/")    
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
