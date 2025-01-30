fd6bafd2bc64e300a06c6f2a91bc316532e4a91b
arthur94a <arthuraraujo94@hotmail.com>
2025-01-29 22:09:49 -0300

    create project and connect to memo database H2

---
+++ src/main/java/br/com/arthuraraujo/todolist/user/UserModel.java
@@ -0,0 +1,11 @@
+package br.com.arthuraraujo.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    
+    private String username;
+    private String name;
+    private String password;
+}
